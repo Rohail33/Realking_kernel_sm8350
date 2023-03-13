c55fcbc864aab6b9f7986b4743e0a8ac36045b14
Rohail33 <rohailyousafzai49@gmail.com>
2023-03-13 20:15:51 +0000

    Build: Add tools

---
+++ anykernel/anykernel.sh
@@ -0,0 +1,28 @@
+
+	# AnyKernel3 Ramdisk Mod Script
+	# osm0sis @ xda-developers
+	properties() { '
+	kernel.string=
+	device.name1=haydn
+        device.name2=haydnin
+        device.name3=alioth
+	do.devicecheck=1
+	do.modules=1
+	do.systemless=1
+	do.cleanup=1
+	do.cleanuponabort=0
+	'; }
+	block=boot;
+	is_slot_device=1;
+	ramdisk_compression=auto;
+	. tools/ak3-core.sh;
+	set_perm_recursive 0 0 755 644 $ramdisk/*;
+	set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+	dump_boot;
+	if [ -d $ramdisk/overlay ]; then
+		rm -rf $ramdisk/overlay;
+	fi;
+	# patch_cmdline firmware_class.path firmware_class.path=/vendor/firmware_mnt/image
+	# patch_cmdline androidboot.selinux androidboot.selinux=permissive
+	# patch_cmdline ramoops_memreserve ramoops_memreserve=8M
+	write_boot;
